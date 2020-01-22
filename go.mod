701907b93cf9b7b5fde0b913d156bf60c84e7fda
kvn <v.kudryavtsev@dev.untill.com>
2020-01-22 18:40:12 +0300

    Using a private action

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/untillpro/ci-action
+
+go 1.13
---
+++ main.go
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2020-present unTill Pro, Ltd. and Contributors
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ */
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Hello world")
+}
