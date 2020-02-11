e577cb4c8dcb3071f83c40c6ec53d80d5b01f01b
kvn <v.kudryavtsev@dev.untill.com>
2020-02-11 18:47:59 +0300

    go-github

--- main.go
+++ main.go
@@ -9,9 +9,36 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
+
+	"github.com/google/go-github/v29/github"
 )
 
 func main() {
+	ctx := context.Background()
+	client := github.NewClient(nil)
+
+	fmt.Println("## Organizations:")
+	orgs, _, err := client.Organizations.List(ctx, "willnorris", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, org := range orgs {
+		fmt.Println(org.GetLogin())
+	}
+
+	fmt.Println("## Repositories:")
+	repos, _, err := client.Repositories.ListByOrg(ctx, "github", nil)
+	if err != nil {
+		println(err)
+		fmt.Println(err)
+		log.Fatal(err)
+	}
+	for _, repo := range repos {
+		fmt.Println(repo.GetName())
+	}
+
 	fmt.Println("Hello world")
 }
