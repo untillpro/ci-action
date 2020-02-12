6caa576cc9a800f74c4757d94d86a8fe468bef67
kvn <v.kudryavtsev@dev.untill.com>
2020-02-12 19:09:02 +0300

    urfave/cli

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/untillpro/ci-action
 
 go 1.13
 
-require github.com/google/go-github/v29 v29.0.3
+require (
+	github.com/google/go-github/v29 v29.0.3
+	github.com/urfave/cli/v2 v2.1.1
+)
--- main.go
+++ main.go
@@ -12,33 +12,56 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"os"
 
 	"github.com/google/go-github/v29/github"
+	"github.com/urfave/cli/v2"
 )
 
 func main() {
+
+	var organization string
+
 	ctx := context.Background()
 	client := github.NewClient(nil)
 
-	fmt.Println("## Organizations:")
-	orgs, _, err := client.Organizations.List(ctx, "willnorris", nil)
+	app := &cli.App{
+		Name:  "ci-action",
+		Usage: "initialize repo for ci-action",
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:        "organization",
+				Value:       "untillpro",
+				Usage:       "GitHub organization",
+				Destination: &organization,
+			},
+		},
+		// Action: func(c *cli.Context) error {
+		// 	return nil
+		// },
+		Commands: []*cli.Command{
+			{
+				Name:    "list",
+				Aliases: []string{"l"},
+				Usage:   "show repository list",
+				Action: func(c *cli.Context) error {
+					fmt.Println("organization", organization)
+					fmt.Println("## Repositories:")
+					repos, _, err := client.Repositories.ListByOrg(ctx, organization, nil)
+					if err != nil {
+						log.Fatal(err)
+					}
+					for _, repo := range repos {
+						fmt.Println(repo.GetName())
+					}
+					return nil
+				},
+			},
+		},
+	}
+
+	err := app.Run(os.Args)
 	if err != nil {
 		log.Fatal(err)
 	}
-	for _, org := range orgs {
-		fmt.Println(org.GetLogin())
-	}
-
-	fmt.Println("## Repositories:")
-	repos, _, err := client.Repositories.ListByOrg(ctx, "github", nil)
-	if err != nil {
-		println(err)
-		fmt.Println(err)
-		log.Fatal(err)
-	}
-	for _, repo := range repos {
-		fmt.Println(repo.GetName())
-	}
-
-	fmt.Println("Hello world")
 }
