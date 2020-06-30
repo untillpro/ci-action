4ab3b6c9605aef951d4cac5c5e9f1da518efd3b8
vitkud <vitkud@gmail.com>
2020-06-30 15:59:06 +0300

    -coverpkg=github.com/untillpro/...

--- dist/index.js
+++ dist/index.js
@@ -590,7 +590,7 @@ async function run() {
 
 				// run Codecov / test
 				if (codecovToken) {
-					await execute('go test ./... -race -coverprofile=coverage.txt -covermode=atomic')
+					await execute('go test ./... -race -coverprofile=coverage.txt -covermode=atomic -coverpkg=github.com/untillpro/...')
 					core.endGroup()
 					core.startGroup('Codecov')
 					await execute(`bash -c "bash <(curl -s https://codecov.io/bash) -t ${codecovToken}"`)
