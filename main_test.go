e5489ea932df8276eaead0059a9b4f0e93a7ec06
vitkud <vitkud@gmail.com>
2021-01-20 18:28:02 +0300

    main_test (fix)

--- main_test.go
+++ main_test.go
@@ -1,4 +1,12 @@
-package main
+/*
+ * Copyright (c) 2020-present unTill Pro, Ltd. and Contributors
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ */
+
+ package main
 
 import (
 	"testing"
