dae1dd22c59e0b47df26bc090b6167e8c7b5a54c
Victor Istratenko <ivvist@gmail.com>
2021-10-02 22:16:31 +0300

    misc

---
+++ scripts/gbash.sh
@@ -0,0 +1,18 @@
+echo "argument: $1"
+
+if [ "$#" -ne 1 ]; then
+    echo "Please add 1 argument: golangci install folder"
+    exit 1	
+fi
+
+echo "Install golangci-lint in folder: $1/bin"
+curl -sfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh| sh -s -- -b $1/bin v1.42.1
+
+echo "Download golangci config"
+curl -s https://raw.githubusercontent.com/untillpro/ci-action/master/scripts/.golangci.yml > /home/runner/work/.golangci.yml
+
+echo "Run linter jobs"
+$1/bin/golangci-lint run        
+
+
+
