30425bb79a371d5daebd9e3dccb806da56c9adb3
ivvist <ivvist@gmail.com>
2021-12-18 17:18:17 +0300

    Auto test bp3-3

--- scripts/rebuild-test-bp3.sh
+++ scripts/rebuild-test-bp3.sh
@@ -4,14 +4,21 @@
 # get current repo name
 fullname=$(git config --local remote.origin.url)
 echo "fullname:$fullname"
+
+oldstr="https:\/\/"
+newstr=""
+reponame=$(echo $fullname | sed "s/$oldstr/$newstr/")
+echo "reponame:$reponame"
+
 pname="${fullname##*/}"
 echo "pname:$pname"
 
 # get airs-bp3 to bp3 folder                                   
-git clone https://ivvheeus:${du}@github.com/untillpro/airs-bp3 bp3
-
+git clone https://ivvheeus:${du}@github.com/untillpro/airs-bp3
 # go to airs-bp3 repo folder
-cd bp3
+cd airs-bp3
+
+go get $reponame
 
 # check if airs-bp23 depends on current repo
 f=$(grep ${pname} go.mod)
