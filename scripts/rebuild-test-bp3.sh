c6e6c097b6e8ce1defb6a4b96846c95daf5c4ad3
ivvist <ivvist@gmail.com>
2021-12-20 16:22:30 +0300

    misc

--- scripts/rebuild-test-bp3.sh
+++ scripts/rebuild-test-bp3.sh
@@ -12,25 +12,35 @@ echo "reponame:$reponame"
 pname="${fullname##*/}"
 echo "pname:$pname"
 
-cd ..
-# get airs-bp3 to bp3 folder                                   
-git clone https://github.com/untillpro/airs-bp3
-# go to airs-bp3 repo folder
-cd airs-bp3
+# check if the repo is not Interface
+needbp3=1
+if [ -z "interface.go" ]; then
+  needbp3=0
+fi
 
-go env -w GOSUMDB=off
+if $needbp3 eq 1; then 
+  echo "The repo is Implementation - it's possible airs-pb3 needed"
+  cd ..
+  # get airs-bp3 to bp3 folder                                   
+  git clone https://github.com/untillpro/airs-bp3
+  # go to airs-bp3 repo folder
+  cd airs-bp3
 
-echo "replace go.mod"
-strreplace="replace $reponame => ../$pname"
-echo  $strreplace >> go.mod
+  go env -w GOSUMDB=off
+  echo "replace go.mod"
+  strreplace="replace $reponame => ../$pname"
+  echo  $strreplace >> go.mod
 
-# check if airs-bp23 depends on current repo
-f=$(grep ${pname} go.mod)
-if [ -z "$f" ]; then
-  echo "No need to test airs-pb3"
+  # check if airs-bp23 depends on current repo
+  f=$(grep ${pname} go.mod)
+  if [ -z "$f" ]; then
+    echo "No need to test airs-pb3"
+  else
+    echo "airs-pb3 will be tested now..."
+    # run tests
+    go test -race ./...
+  fi
 else
-  echo "airs-pb3 will be tested now..."
-  # run tests
-  go test -race ./...
+  echo "The repo is Interface - no airs-pb3"
 fi
 
