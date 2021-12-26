0aec92b7013e4dbc4497fd67d85d8fa70a5af706
ivvist <ivvist@gmail.com>
2021-12-26 15:42:02 +0300

    BP3 push with new denepdency is implemented

--- scripts/rebuild-test-bp3.sh
+++ scripts/rebuild-test-bp3.sh
@@ -48,7 +48,8 @@ fi
 if [[ "$gh_event" == "push" ]];then 
   git config --global user.email "ivvist@gmail.com"
   git config --global user.name "ivvist"
-  git remote set-url origin https://$github_token@github.com/untillpro/airs-bp3.git
+  git config --global url."https://${github_token}@github".insteadOf "https://github"
+#  git remote set-url origin https://${github_token}@github.com/untillpro/airs-bp3.git
   git add .
   git commit -m "$reponame is updated"
   git push
