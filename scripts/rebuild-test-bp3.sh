9b59d1e010c66072bcae08ef80524cc73c84fe4b
ivvist <ivvist@gmail.com>
2021-12-26 12:33:35 +0300

    Split CI push/pr

--- scripts/rebuild-test-bp3.sh
+++ scripts/rebuild-test-bp3.sh
@@ -13,25 +13,31 @@ pname="${fullname##*/}"
 echo "pname:$pname"
 
 # check if the repo is not Interface
-  echo "The repo is Implementation - it's possible airs-pb3 needed"
-  cd ..
-  # get airs-bp3 to bp3 folder                                   
-  git clone https://github.com/untillpro/airs-bp3
-  # go to airs-bp3 repo folder
-  cd airs-bp3
+echo "The repo is Implementation - it's possible airs-pb3 needed"
+cd ..
+# get airs-bp3 to bp3 folder                                   
+git clone https://github.com/untillpro/airs-bp3
+# go to airs-bp3 repo folder
+cd airs-bp3
 
+echo "gh_event: $gh_event"
+if [[ "$gh_event" == "push" ]];then 
   go env -w GOSUMDB=off
+  echo "go get $reponame"
+  go get $reponame
+else
   echo "replace $reponame => ../$pname"
   strreplace="replace $reponame => ../$pname"
   echo  $strreplace >> go.mod
+fi
 
-  # check if airs-bp23 depends on current repo
-  f=$(grep ${pname} go.mod)
-  if [ -z "$f" ]; then
-    echo "No need to test airs-pb3"
-  else
-    echo "airs-pb3 will be tested now..."
-    # run tests
-    go test -race ./...
-  fi
+# check if airs-bp23 depends on current repo
+f=$(grep ${pname} go.mod)
+if [ -z "$f" ]; then
+  echo "No need to test airs-pb3"
+else
+  echo "airs-pb3 will be tested now..."
+  # run tests
+  go test -race ./...
+fi
 
