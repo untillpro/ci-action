ec78245bda139eeda9d299df4773b2dec51a0e15
ivvist <ivvist@gmail.com>
2022-06-28 16:39:31 +0300

    Fix in release ce workdlow

--- scripts/git-release.sh
+++ scripts/git-release.sh
@@ -15,10 +15,8 @@ text=""
 # Get pre-release flag
 prerelease="${ver#*$s}"
 prerelease="${prerelease//[[:blank:]]/}"
-new_branchname="rc"
 pr=true
 if [[ ${version} == ${prerelease} ]];then
-   new_branchname="release"
    pr=false
    shver=${version}
    shrel=""
@@ -46,32 +44,12 @@ prev_branchname=$branchname
 
 repo_full_name=$(git config --get remote.origin.url | sed 's/.*:\/\/github.com\///;s/.git$//')
 
-# delete existing rc branch - not needed old one
-git push origin :$new_branchname
-
-# set upstream to parent heeus repo 
-git remote add upstream https://github.com/${repo_full_name}
-
-# get last code from upstream
-git pull -p upstream ${prev_branchname}
-
-git stash
-# create devbranch with topic number
-git checkout -b ${new_branchname}
-
-# get last from newbranch
-git pull -p -r origin ${new_branchname}
-
-git push -u origin ${new_branchname}
-
-branch=${new_branchname}
-echo "Create release $version for repo: $repo_full_name branch: $branch"
 generate_post_data()
 {
   cat <<EOF
 {
   "tag_name": "${version}",
-  "target_commitish": "${branch}",
+  "target_commitish": "${branchname}",
   "name": "${version}",
   "body": "${text}",
   "draft": false,
@@ -80,12 +58,12 @@ generate_post_data()
 EOF
 }
 
-id=$(curl -H "Accept: application/json" \
+release_id=$(curl -H "Accept: application/json" \
    -H "Authorization: token ${token}" \
    -d "$(generate_post_data)" \
    https://api.github.com/repos/$repo_full_name/releases | jq -r '.id')
 
-if [ -z ${id} ] || [[ ${id} == null ]]; then
+if [ -z ${release_id} ] || [[ ${release_id} == null ]]; then
   echo "release_id is empty"
   exit 1
 fi
@@ -107,7 +85,7 @@ for fname in *
 do
    if [[ ${fname} != "." ]];then
      echo "File $fname is uploading..."
-     url="https://uploads.github.com/repos/$repo_full_name/releases/${id}/assets?name=$(basename $fname)"
+     url="https://uploads.github.com/repos/$repo_full_name/releases/${release_id}/assets?name=$(basename $fname)"
      echo "url: $url"
      curl \
       -H "Authorization: token ${token}" \
@@ -115,8 +93,5 @@ do
       --data-binary @$fname ${url}
    fi
 done
-
-# Switch to original branch
-git checkout -b ${old_branchname}
 
 echo "All files are uploaded"
