4eba7a812534226936507ba7e2e5bb357e93d2bb
ivvist <ivvist@gmail.com>
2022-10-22 12:08:47 +0200

    Add scripts for BP3 release

---
+++ scripts/BP3Realease/cd_RunR.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Clone from branch "main" last code and push to branch "rc"
+
+if [ "$#" -ne 2 ]; then
+    echo "Please add 2 arguments: repo name, action type(new/fix)"
+    read -p "Press [Enter] to exit."
+    exit 1	
+fi
+
+org="untillpro"
+repodir=$1
+if [[ $2 == "new" ]]; then 
+  oldbranch="rc"
+  newbranch="release"
+else
+  oldbranch="release"
+  newbranch="rfix"
+fi
+
+sh fix_branch.sh $org $repodir $oldbranch $newbranch $2
+
+read -p "Press [Enter] to complete RC"
---
+++ scripts/BP3Realease/cd__NewRC.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# GO project
+sh cd_RunRC.sh airs-bp3 new
+
