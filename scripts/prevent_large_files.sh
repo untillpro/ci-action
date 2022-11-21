85fd6da0c53f28d68780d6f50c04a35b7ca03b8b
ivvist@gmail.com <ivvist@gmail.com>
2022-11-21 11:29:17 +0100

    misc

--- scripts/prevent_large_files.sh
+++ scripts/prevent_large_files.sh
@@ -3,10 +3,10 @@
 fc=$1
 fs=$2
 
-if [ $fs -gt 2 ]
+if [ $fc -gt 2 ]
 then
 	cat <<\EOF
-	Error: Attempt to Commit too much files
+	Error: Attempt to Commit too much files: $fc
 
 EOF
 	exit 1
@@ -15,7 +15,7 @@ fi
 if [ $fs -gt 1000 ]
 then
 	cat <<\EOF
-	Error: Attempt to Commit large file
+	Error: Attempt to Commit large file: $fs
 
 EOF
 	exit 1
