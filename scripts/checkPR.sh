5ec07fa607b4ec38cf9f25a174ccc91fe6586da7
ivvist <ivvist@gmail.com>
2022-12-06 18:41:52 +0100

    check files size in pull request

---
+++ scripts/checkPR.sh
@@ -0,0 +1,50 @@
+#!/bin/bash                    
+                                           
+jqbase64 () {
+  echo "$file" | base64 -d | jq -r "$1"
+}
+
+doCheckPR ()  {
+
+  overalsize=100000
+  singlelimit=100000
+  cntlimit=200  
+
+  header="Accept: application/vnd.github.v3+json"
+  repo_full_name=$(git config --get remote.origin.url | sed 's/.*:\/\/github.com\///;s/.git$//')
+  files="api.github.com/repos/$repo_full_name/pulls/$pr_number/files"
+  body=$(curl -sSL -H "Authorization: $GITHUB_TOKEN" -H "$header" "$files")
+
+  ln=0
+  maxln=0
+  maxfile=""
+  fcnt=0
+  for file in $(echo "$body" | jq -r '.[] | @base64'); do
+    fn=$(jqbase64 '.filename') 
+    fc=$(jqbase64 '.patch') 
+    fc=${fc#*@@}
+    fc=${fc#*@@}
+    newln=${#fc}
+    if [ $newln -gt $maxln ];then
+	maxln=$newln
+        maxfile=$fn
+    fi
+    ((ln +=$newln))
+    ((fcnt=fcnt+1))
+  done
+
+  if [ $ln -gt $overalsize ];then
+	echo "::error::Total size $ln of changed files exceeds limt $overalsize bytes"
+	exit 1
+  fi
+  if [ $maxln -gt $singlelimit ];then
+	echo "::error::Largest file '$maxfile' has size $maxln. It exceeds limit $singlelimit bytes"
+	exit 1
+  fi
+  if [ $fcnt -gt $cntlimit ];then
+	echo "::error::Number of files $fcnt in Pull request exceeds limit $cntlimit files"
+	exit 1
+  fi
+}
+
+doCheckPR     
