4a8a22f1c56b1ed978338a673bdbf37fd349415d
ivvist <ivvist@gmail.com>
2023-02-02 13:28:50 +0100

    Fix for workflow for node projects with submodules

--- scripts/test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-auth_header="$(git config --local --get http.https://github.com/.extraheader)"
-git submodule sync --recursive
-git -c "http.extraheader=$auth_header" -c protocol.version=2 submodule update --init --force --recursive --depth=1
-git config --global url."https://".insteadOf ssh://
-
