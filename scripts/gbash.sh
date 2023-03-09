d22f900cc3e879f457dd0665c4bff7a0a3cabab3
ivvist <ivvist@gmail.com>
2023-03-09 15:03:40 +0100

    disable cyclop

--- scripts/gbash.sh
+++ scripts/gbash.sh
@@ -18,7 +18,7 @@ status="$?"
 
 if [ ${status} -eq 0 ]; then 
   echo "Install cyclop"
-  go get github.com/untillpro/cyclop/cmd/cyclop@v1.2.103
+  go install github.com/untillpro/cyclop/cmd/cyclop@v1.2.103
 
 #  echo "Run cyclop"
 #  $(go env GOPATH)/bin/cyclop -skipSwitch=true -skipTests=true -maxComplexity 12 ./...
