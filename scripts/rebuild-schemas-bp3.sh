5d03d2e2ff08767898dc922117bf1932a4d8b27c
ivvist <ivvist@gmail.com>
2024-03-17 19:58:26 +0100

    baseline schemas for pb3

---
+++ scripts/rebuild-schemas-bp3.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+  cd airs-bp3
+echo "making build airs-bp3"
+  go build -o airs-bp airsbp3/cli/*.go
+echo "rebuild baseline schemas"
+  ./airs-bp baseline_schemas airsbp3/baseline_schemas
+  git config --local user.email $commit_email
+  git config --local user.name $commit_user
+echo "commit and push new baskine schemas"
+  git add .
+  git commit -m "baseline schemas update"
+  git push	  
