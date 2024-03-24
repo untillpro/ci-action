e366f06e0fb6f5f3d4ebb28de8baa3b5a79b6ef9
ivvist <ivvist@gmail.com>
2024-03-24 16:55:41 +0100

    update voedger dependence in bp3

--- scripts/update-bp3-voedger.sh
+++ scripts/update-bp3-voedger.sh
@@ -3,7 +3,7 @@
 go env -w GOSUMDB=off
 echo "gh_event: $gh_event"
 if [[ "$gh_event" == "workflow_dispatch" ]]; then 
-  go get github.com/voegder/voedger"
+  go get github.com/voegder/voedger
   echo "go mod tidy"
   go mod tidy
 
