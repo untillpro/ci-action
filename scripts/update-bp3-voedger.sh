c713a248cd70a464e6e45fab8367bca1cdf1ff81
ivvist <ivvist@gmail.com>
2024-03-24 16:49:21 +0100

    update voedger dependance in bp3

--- scripts/update-bp3-voedger.sh
+++ scripts/update-bp3-voedger.sh
@@ -3,7 +3,14 @@
 go env -w GOSUMDB=off
 echo "gh_event: $gh_event"
 if [[ "$gh_event" == "push" ]]; then 
-  echo "go get github.com/voegder/voedger"
+  go get github.com/voegder/voedger"
   echo "go mod tidy"
   go mod tidy
+
+  git config --local user.email $commit_email
+  git config --local user.name $commit_user
+  echo "commit and push new backine schemas"
+  git add .
+  git commit -m "voedger: $message"
+  git push	  
 fi
