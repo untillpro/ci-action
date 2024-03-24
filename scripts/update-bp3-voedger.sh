1d4b6a27c1da5b749bb233d65a9da2dbb8090c6f
ivvist <ivvist@gmail.com>
2024-03-24 17:16:21 +0100

    update voedger dependence in bp3

--- scripts/update-bp3-voedger.sh
+++ scripts/update-bp3-voedger.sh
@@ -2,15 +2,20 @@
 
 go env -w GOSUMDB=off
 echo "gh_event: $gh_event"
+
 if [[ "$gh_event" == "workflow_dispatch" ]]; then 
+
+  header="Accept: application/vnd.github+json"
+  repo_full_name="https://api.github.com/repos/voedger/voedger/commits/main"
+  last_commit=$(curl -s -u "${github_token}:x-oauth-basic" -H $header $repo_full_name | jq -r '.commit.message')
+
   go get github.com/voegder/voedger
-  echo "go mod tidy"
   go mod tidy
 
+  last_commit
   git config --local user.email $commit_email
   git config --local user.name $commit_user
-  echo "commit and push new backine schemas"
   git add .
-  git commit -m "voedger: $message"
+  git commit -m "voedger: $last_commit"
   git push	  
 fi
