a193d8576b9854b4f739c3e7ec443183dc954434
ivvist <ivvist@gmail.com>
2024-04-24 09:24:22 +0200

    label for issue in createissue.sh

--- scripts/createissue.sh
+++ scripts/createissue.sh
@@ -5,16 +5,18 @@ REPO=$repo
 TITLE=$name
 BODY=$body
 ASSIGNEE=$assignee 
+LABEL=$label 
 
 echo "REPO :    $repo"
 echo "TITLE:    $name"
 echo "BODY :    $body"
 echo "ASSIGNEE: $assignee"
+echo "LABEL:    $label"
 
 if [ -z "$REPO" ] || [ -z "$TITLE" ] || [ -z "$BODY" ] || [ -z "$ASSIGNEE" ]; then
     echo "Error: Issue not created because some patrameters are empty."
     exit 0
 fi
 # Create the issue and assign it
-gh issue create --repo "$REPO" --title "$TITLE" --body "$BODY" --assignee "$ASSIGNEE"
+gh issue create --repo "$REPO" --title "$TITLE" --body "$BODY" --assignee "$ASSIGNEE" --label "$LABEL"
 
