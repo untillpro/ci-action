3f20367ebf80f452b9303b7f141ab800a6016ddb
ivvist <ivvist@gmail.com>
2024-08-23 17:09:48 +0300

    golangci-lint version->1.60.3

--- scripts/gbash.sh
+++ scripts/gbash.sh
@@ -6,7 +6,7 @@ if [ "$#" -ne 1 ]; then
 fi
 
 echo "Install golangci-lint in folder: $1/bin"
-curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(go env GOPATH)/bin v1.55.2
+curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(go env GOPATH)/bin v1.60.3
 
 golangci-lint --version
 
