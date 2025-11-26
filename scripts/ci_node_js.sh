8f4292049c4097956a0fa6cc31ddba433551a5e9
Denis Gribanov <d.gribanov@dev.untill.com>
2025-11-26 15:51:29 +0300

    build nodejs projects fix

--- scripts/ci_node_js.sh
+++ scripts/ci_node_js.sh
@@ -3,21 +3,8 @@ set -Eeuo pipefail
 
 # Node.js language-specific CI logic, extracted from ci_main.sh
 
-echo "Node.js project detected"
-
 echo "::group::Build"
 npm install
 npm run build --if-present
 npm test
-
-# Run codecov if token provided
-if [ -n "$CODECOV_TOKEN" ]; then
-    echo "::endgroup::"
-    echo "::group::Codecov"
-    npm install -g codecov
-    istanbul cover ./node_modules/mocha/bin/_mocha --reporter lcovonly -- -R spec
-    codecov --token="$CODECOV_TOKEN"
-fi
-
 echo "::endgroup::"
-
