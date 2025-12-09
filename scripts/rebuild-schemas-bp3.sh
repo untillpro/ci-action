6b85c8289cf3debcce319f9d816b1c747e296efd
Denis Gribanov <d.gribanov@dev.untill.com>
2025-12-09 17:39:47 +0300

    [AIR-2462] eliminate unused scripts

--- scripts/rebuild-schemas-bp3.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -Eeuo pipefail
-
-echo "making build airs-bp3"
-  go build -o airs-bp airsbp3/cli/*.go
-echo "rebuild baseline schemas"
-  ./airs-bp baseline_schemas airsbp3/baseline_schemas
